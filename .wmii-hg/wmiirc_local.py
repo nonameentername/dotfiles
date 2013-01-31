ef80acd7795617c649b4465b9456da9c5b31f3ff
werner mendizabal <nonameentername@gmail.com>
2013-01-31 14:07:06 -0600

    add battery monitor to wmii

--- .wmii-hg/wmiirc_local.py
+++ .wmii-hg/wmiirc_local.py
@@ -19,6 +19,7 @@ wmii['font'] = 'drift,-*-fixed-medium-r-*-*-12-*-*-*-*-*-*-*'
 # Colors tuples: "<text> <background> <border>"
 wmii['focuscolors'] = '#ffffff', '#555555', '#777777'
 wmii['normcolors'] = '#bbbbbb', '#222222', '#000000'
+wmii['alertcolors'] = '#ff0000', '#222222', '#777777'
 wmii['border'] = 1
 
 keys.bind('main', (
@@ -42,3 +43,30 @@ keys.bind('main', (
 @defmonitor
 def time(self):
     return wmii.cache['focuscolors'], datetime.datetime.now().strftime('%a %b %d %I:%M:%S %Y')
+
+def get_battery_status(percent):
+        equal = '=' * (int(percent) / 10)
+        minus = '-' * (10 - int(percent) / 10)
+        return '[%s%s] %d.0' % (equal, minus, percent)
+
+@defmonitor
+def battery(self):
+    power_dir = '/sys/class/power_supply/BAT0/'
+
+    if os.path.exists(power_dir):
+        charge_now_filename = os.path.join(power_dir, 'charge_now')
+        charge_now = open(charge_now_filename).readline().strip()
+
+        charge_full_filename = os.path.join(power_dir, 'charge_full')
+        charge_full = open(charge_full_filename).readline().strip()
+
+        percent = min((float(charge_now) / float(charge_full) * 100), 100.0)
+
+        battery_status = get_battery_status(percent)
+
+        if percent < 10:
+            return wmii.cache['alertcolors'], battery_status
+        else:
+            return wmii.cache['focuscolors'], battery_status
+    else:
+        return wmii.cache['focuscolors'], get_battery_status(100)
